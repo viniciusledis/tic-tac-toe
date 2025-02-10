224a267bcc3ce20358a0c953591fad9bfdba68e8
Vinicius Ledis <vinicius.ledis@gmail.com>
2025-02-10 18:02:36 -0300

    Primeiro commit - subindo projeto

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,10 @@
+export default function GameOver({ winner, onRematch }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over!</h2>
+      {winner && <p>{winner} Won!</p>}
+      {!winner && <p>It's a draw!</p>}
+      <button onClick={onRematch}>Rematch</button>
+    </div>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onNameChange,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onNameChange(symbol, playerName);
+    }
+  }
+
+  function handleNameChange(event) {
+    const value = event.target.value;
+    setPlayerName(value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleNameChange}
+            required
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
